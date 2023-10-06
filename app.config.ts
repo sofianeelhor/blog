ee5159901002c5f03f55b0668145990908f1387e
22sh <43454096+sofianeelhor@users.noreply.github.com>
2023-10-06 22:12:59 +0200

    added repo

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'The Infosec Hangout',
+    description: '22sh’s infoSec blog: Dive into Write-ups & shenanigans.',
     image: {
       src: '/banner.png',
       alt: 'An image showcasing my project.',
@@ -9,33 +9,42 @@ export default defineAppConfig({
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/favicon.png', // path of the logo
+        pathDark: '/favicon.png', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: '0x22.sh' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: '0x22sh',
+      instagram: '',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      github: 'sofianeelhor'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
+    }
+  },
+
+  prose: {
+    h1: {
+      icon: ''
     }
   }
 })
